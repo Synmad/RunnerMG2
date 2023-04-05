f791e15adc7e25ad48cf90f51391909f750ec102
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-04-05 19:50:17 -0300

    v0.0.1
    Player controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,5 +10,31 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (transform.position.x == -3)
+            {
+                transform.position = new Vector3(0,0, transform.position.z);
+            }
+            else if (transform.position.x == 0) 
+            {
+                transform.position = new Vector3(3,0, transform.position.z);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (transform.position.x == 3)
+            {
+                transform.position = new Vector3(0, 0, transform.position.z);
+            }
+            else if (transform.position.x == 0) 
+            { 
+                transform.position = new Vector3(-3, 0, transform.position.z);
+            }
+
+
     }
+}
 }
